7239cd21a33fbc3ce7a6fed8ec3b87a18acd164b
Avez Shariq <avezshariq@gmail.com>
2024-05-18 07:00:19 +0530

    add button

--- functionality.py
+++ functionality.py
@@ -1,33 +1,129 @@
 import pygame
+import random
 
-# Decorator function
-def update_and_quit(func):
-    def wrapper(*args, **kwargs):
-        clock = pygame.time.Clock()
-        app_running = True
+
+class Button:
+    font_size = 30
+    rect_color = (200, 200, 200)
+    text_color = (0, 0, 0)
+    default_color = (200, 200, 200)
+    hover_color = (255, 255, 255)
+    click_color = (255, 0, 0)
+    rect_height = 60
+
+    def __init__(self, text, position):
+        self.text = text
+        self.position = position
+        self.rect_width = len(self.text) * 12
+        self.rect_x = self.position[0] - self.rect_width / 2
+        self.rect_y = self.position[1] - self.rect_height / 2
+        self.clicked = False
+
+    def draw(self, screen):
+        rect = pygame.Rect(self.rect_x, self.rect_y, self.rect_width, self.rect_height)
+        pygame.draw.rect(screen, self.rect_color, rect)
+        draw_text(screen=screen, text=self.text, position=self.position)
+        if self.clicked:
+            pygame.draw.line(
+                screen,
+                (0, 0, 0),
+                (self.rect_x, self.rect_y + self.rect_height),
+                (self.rect_x + self.rect_width, self.rect_y + self.rect_height),
+                5,
+            )
+
+    def hover(self):
+        mouse_pos = pygame.mouse.get_pos()
+        if (
+            self.rect_x < mouse_pos[0] < self.rect_x + self.rect_width
+            and self.rect_y < mouse_pos[1] < self.rect_y + self.rect_height
+        ):
+            self.rect_color = self.hover_color
+        else:
+            self.rect_color = self.default_color
+
+    def click(self, screen):
+        mouse_pos = pygame.mouse.get_pos()
+        if (
+            self.rect_x < mouse_pos[0] < self.rect_x + self.rect_width
+            and self.rect_y < mouse_pos[1] < self.rect_y + self.rect_height
+        ):
+            print("I am clicked")
+            self.clicked = True
+
+
+def draw_text(
+    screen: pygame.Surface,
+    size: int = 30,
+    text: str = "Hola",
+    color: tuple = (0, 0, 0),
+    position: tuple = (0, 0),
+):
+    text_font = pygame.font.Font("resources/Unitblock-JpJma.ttf", size)
+    text = text_font.render(text, True, color)
+    text_rect = text.get_rect()
+    text_rect.center = position
+    screen.blit(text, text_rect)
+
+
+def splash_screen(screen: pygame.Surface, FPS: int):
+    app_running = True
+    clock = pygame.time.Clock()
+    counter = 0
+    while app_running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == pygame.KEYDOWN:
                 app_running = False
-        dt = clock.tick(24)
-        a = func(app_running, *args, **kwargs)
-        pygame.display.flip()   
-        return a
-    return wrapper
+
+        splash = pygame.image.load("assets/bg.png")
+        screen.blit(splash, (0, 0))
+        draw_text(
+            size=50,
+            text="Know your",
+            color=(255, 255, 255),
+            position=(630, 210),
+            screen=screen,
+        )
+        draw_text(
+            size=150,
+            text="Solar System",
+            color=(255, 255, 255),
+            position=(680, 300),
+            screen=screen,
+        )
+        if counter < 1 * FPS:
+            draw_text(
+                size=50,
+                text="Press any key to continue",
+                color=(200, 200, 200),
+                position=(650, 500),
+                screen=screen,
+            )
+        if counter > 1.5 * FPS:
+            counter = 0
+        counter += 1
+
+        dt = clock.tick(FPS)
+        pygame.display.flip()
 
 
+def menu(screen: pygame.Surface, FPS: int):
+    app_running = True
+    clock = pygame.time.Clock()
+    b1 = Button(text="Click here for all the help you need", position=(640, 100))
 
-@update_and_quit
-def splash_screen(app_running, screen):
-    splash_running  = True
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                splash_running = False
-    splash = pygame.image.load('assets/bg.png')
-    screen.blit(splash, (0, 0))
-    solar_system_font = pygame.font.Font('resources/Unitblock-JpJma.ttf', 100)
-    solar_system = solar_system_font.render('Solar System', True, (255, 255, 255))
-    solar_system_rect = solar_system.get_rect()
-    solar_system_rect.center = (640, 300)
-    screen.blit(solar_system, solar_system_rect)
-    return app_running, splash_running
+    while app_running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                b1.click(screen)
+
+        screen.fill((255, 255, 0))
+        b1.hover()
+        b1.draw(screen=screen)
+
+        dt = clock.tick(FPS)
+        pygame.display.flip()
--- game.py
+++ game.py
@@ -5,12 +5,9 @@ pygame.init()
 
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption('Solar System')
+FPS = 24
 
-app_running = True
-while app_running:
-    splash_running = True
-    while splash_running:
-        app_running, splash_running = splash_screen(screen)
-    
+# splash_screen(screen=screen, FPS=FPS)
+menu(screen=screen, FPS=FPS)
 
 pygame.quit()
